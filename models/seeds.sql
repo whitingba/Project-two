b1fbb944d5b98e8f8b59dbf6dc81cf7fbc0e8239
Barbara Whiting <whitingba@gmail.com>
2019-09-28 11:36:43 -0600

    created seeds

---
+++ models/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO tasks
+    (task, frequency)
+VALUES
+    ('Inspect HVAC filter, replace if dirty', 'Monthly');
+INSERT INTO tasks
+    (task, frequency)
+VALUES
+    ('Clean kitchen sink disposal', 'Monthly');
+INSERT INTO tasks
+    (task, frequency)
+VALUES
+    ('Inspect fire extinguisher', 'Monthly');
