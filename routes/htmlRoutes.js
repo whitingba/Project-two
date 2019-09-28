d982924f2b22faf56eaa8c452c08849f6e8e0b7f
Obiageli Ogbutor <obygirl81@gmail.com>
2019-09-28 13:46:04 -0600

    uploading initial htmlRoutes.js file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,23 @@
 var db = require("../models");
 
+var path = require("path");
+
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Tasks.findAll({}).then(function(dbTasks) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        tasks: dbTasks
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  // Load task page and pass in an example by id
+  app.get("/task/:id", function(req, res) {
+    db.Tasks.findOne({ where: { id: req.params.id } }).then(function(dbTasks) {
+      res.render("task", {
+        tasks: dbTasks
       });
     });
   });
@@ -25,3 +27,34 @@ module.exports = function(app) {
     res.render("404");
   });
 };
+
+// Index route to pull all tasks loads file.html
+ app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/ADDFILENAME????.html"));
+});
+
+  // add route loads the add.html page, where users can enter new tasks to the db
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/ADDFILENAME????.html"));
+  });
+
+  // outdoor route loads the outdoor.html page, where outdoor tasks in the db are displayed
+  app.get("/outdoor", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/ADDFILENAME?????.html"));
+  });
+
+  // indoor route loads the indoor.html page, where outdoor tasks in the db are displayed
+  app.get("/indoor", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/ADDFILENAME?????.html"));
+  });
+
+  // user route loads the user.html page, where user tasks in the db are displayed
+  app.get("/user", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/ADDFILENAME?????.html"));
+  });
+
+  // login route loads the login.html page, where user can log in the db are displayed
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/ADDFILENAME?????.html"));
+  });
+
