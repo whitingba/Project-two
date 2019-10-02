a0313698cf3371f86f81a1db88824a51ef86f876
Obiageli Ogbutor <obygirl81@gmail.com>
2019-10-02 14:27:53 -0600

    manually added data to database to test out route and added new user route to add user

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var db = require("../config/database");
 var User = require("../models/user");
 
+//  This will get user list
 router.get("/", (req, res) => 
    User.findAll()
    .then(users => {
@@ -10,5 +11,24 @@ router.get("/", (req, res) =>
     res.sendStatus(200);
  })
    .catch(err => console.log(err)));
+
+
+// This route will add a user = where we add our post request
+router.get("/add", (req, res) => {
+    var data = {
+        user_name: "Oby1",
+        contact_email: "user1@gmail.com"
+    }
+var {user_name, contact_email} = data;
+
+//insert into table
+User.create({
+    user_name,
+    contact_email 
+})
+ .then(gig => res.redirect("/users"))
+ .catch(err => console.log(err));
+
+});
 
 module.exports = router;
