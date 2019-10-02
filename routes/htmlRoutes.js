9c3e48189e483a43bc98f30176589d9c01b0dc5c
Phil Mauracher <gradientus@gmail.com>
2019-10-02 16:02:00 -0600

    Updated htmlroutes to start using handlebars.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,32 +1,61 @@
 var db = require("../models");
-
 var path = require("path");
 
-module.exports = function (app) {
-  // Load index page
-  app.get("/", function (req, res) {
-    db.Tasks.findAll({}).then(function (dbTasks) {
-      res.render("index", {
-        msg: "Welcome!",
-        tasks: dbTasks
-      });
-    });
+module.exports = function(app) {
+  // Load login page
+  app.get("/", function(req, res) {
+    //res.sendFile(path.join(__dirname, "../phil/login.html"));
+    res.render("login");
   });
 
-  // Load task page and pass in an example by id
-  app.get("/task/:id", function (req, res) {
-    db.Tasks.findOne({ where: { id: req.params.id } }).then(function (dbTasks) {
-      res.render("task", {
-        tasks: dbTasks
-      });
-    });
+  // Load signup page
+  app.get("/signup", function(req, res) {
+    res.render("signup");
+  });
+
+  // Load add task page
+  app.get("/add/task", function(req, res) {
+    res.render("addtask");
+  });
+
+  //TODO: Take a harder look at these 2 edit task pages  they both go to edittask handlebars
+  //Load edit task page  (which is just the addtask page with data populated)
+  app.get("/edit/task/", function(req, res) {
+    res.render("edittask");
+  });
+
+  //TODO: Take a harder look at these 2 edit task pages  they both go to edittask handlebars
+  //Load the addtask page with datapopulated so the user can edit
+  app.get("/edit/task/:taskid", function(req, res) {
+    res.render("addtask");
+  });
+
+  //Load list all tasks page
+  app.get("/list/task", function(req, res) {
+    res.render("listtask");
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
+
+//NOTE://///////////     Keeping all this just incase we might need it     ////////////////////
+//   // Load task page and pass in an example by id
+//   app.get("/task/:id", function (req, res) {
+//     db.Tasks.findOne({ where: { id: req.params.id } }).then(function (dbTasks) {
+//       res.render("task", {
+//         tasks: dbTasks
+//       });
+//     });
+//   });
+
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function (req, res) {
+//     res.render("404");
+//   });
+// };
 
 // // Index route to pull all tasks loads file.html
 // app.get("/", function(req, res) {
