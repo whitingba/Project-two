9b4a9758b94f47a90ee7c883765157e7a415c3a9
Barbara Whiting <whitingba@gmail.com>
2019-10-01 20:37:01 -0600

    update apiroutes.js with assignments for Barbara and Obi

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,10 @@
 // Import the model to use its database functions.
 var db = require("../models");
 
-module.exports = function(app) {
-
+module.exports = function (app) {
+  //****************ALL TASK ROUTES (BARBARA)***************/
   // GET route for getting all of the tasks
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     db.Tasks.findAll({
       order: [
         ['task_name', 'ASC'],
@@ -14,63 +14,66 @@ module.exports = function(app) {
           model: db.Users
         }
       ],
-    }).then(function(tasks) {
+    }).then(function (tasks) {
       // We have access to the tasks as an argument inside of the callback function
-      console.log(burgers);
-      db.Users.findAll().then(function(users){
-        res.render("index",{tasks,users});
+      console.log(tasks);
+      db.Users.findAll().then(function (users) {
+        res.render("index", { tasks, users });
       });
-     
+
     });
   });
 
-  app.post("/api/tasks", function(req, res) {
+  app.post("/api/tasks", function (req, res) {
     db.Tasks.create({
       task_name: req.body.task_name,
       UserId: req.body.UserId
-    }).then(function(results) {
+    }).then(function (results) {
       res.json(results);
-    }).catch(function(error) {
+    }).catch(function (error) {
       return res.status(500).send(error);
-    }); 
+    });
   });
 
-  app.post("/api/user", function(req, res) {
+  app.post("/api/user", function (req, res) {
     db.Users.create({
       user_name: req.body.user_name,
-    }).then(function(results) {
+    }).then(function (results) {
       res.json(results);
-    }).catch(function(error) {
+    }).catch(function (error) {
       return res.status(500).send(error);
-    }); 
+    });
   });
 
-  app.put("/api/tasks/:id", function(req, res) {
+  app.put("/api/tasks/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-  
+
     db.Tasks.update({
       frequency: req.body.frequency,
     }, {
       where: {
         id: req.params.id
       }
-    }).then(function(results) {
+    }).then(function (results) {
       res.json(results);
     });
   });
 
-  app.delete("/api/tasks/:id", function(req, res) {
+  app.delete("/api/tasks/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-  
+
     db.Tasks.destroy({
       frequency: req.body.frequency,
     }, {
       where: {
         id: req.params.id
       }
-    }).then(function(results) {
+    }).then(function (results) {
       res.json(results);
     });
   });
-  
+
+
+
+  //**********************ALL USER ROUTES (OBI)*********************/
 }
