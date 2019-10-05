2d1541d38244c40f214a28f6a1bf95a0afee2fb4
Mr. T <c.austin.terry@gmail.com>
2019-10-05 12:04:01 -0600

    fixed add user js files

--- public/js/adduser.js
+++ public/js/adduser.js
@@ -1,38 +1,48 @@
 // The API object contains methods for each kind of request we'll make
 
-var API = {
-  saveUser: function(userName, password) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "/api/users", //NOTE: REPLACE THE ADD TASK ROUTE HERE
-      data: JSON.stringify(userName, password)
-    });
-  }
-};
-
-// Add event listeners to the submit and delete buttons
-$("#submit").on("click", function() {
-  event.preventDefault();
-  console.log("Ahhhhhh!");
-  userName = $("#user-entry")
-    .val()
-    .trim();
-  password = $("#user-password")
-    .val()
-    .trim();
-  console.log(userName, password);
-  //Show a modal if there is no info, but the Add Task button was clicked
-  // if (userName === "") {
-  //   $(".modal").modal("toggle");
-
-  //   //add the task to the database
-  // } else {
-
-  //   });
-  API.saveUser(userName, password).then(function() {
+$(document).ready(function() {
+  // Add event listeners to the submit and delete buttons
+  $("#submit").on("click", function() {
+    event.preventDefault();
+    console.log("Ahhhhhh!");
+    userName = $("#user-entry")
+      .val()
+      .trim();
+    password = $("#user-password")
+      .val()
+      .trim();
     console.log(userName, password);
+
+    var userObject = {
+      userName: userName,
+      password: password
+    };
+    //Show a modal if there is no info, but the Add Task button was clicked
+    if (userName === "") {
+      console.log(userName);
+      $(".modal").modal("toggle");
+    } else {
+      $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "/api/users",
+        data: JSON.stringify(userObject),
+        dataType: "json",
+        error: function(err) {
+          console.log("Fucking error: ", err);
+        }
+      });
+
+      function resetUser() {
+        $("#user-entry").val("");
+        $("#user-password").val("");
+      }
+      resetUser();
+    }
+
+    //   });
+    // API.saveUser(userName, password).then(function() {
+    //   console.log(userName, password);
+    // });
   });
 });
