1604b98cb5d4ee8603d993a8bbcc3b3e107568c2
Mr. T <c.austin.terry@gmail.com>
2019-10-06 16:55:56 -0600

    pull commit

--- public/js/listuser.js
+++ public/js/listuser.js
@@ -1,7 +1,7 @@
 // Get references to page elements
 $(document).ready(function() {
   var $userList = $("#user-list");
-
+  var taskData;
   // The API object contains methods for each kind of request we'll make
   var API = {
     getUsers: function() {
@@ -10,61 +10,54 @@ $(document).ready(function() {
         type: "GET"
       });
     }
-    // deleteUsers: function(id) {
-    //   return $.ajax({
-    //     url: "api/users/" + id,
-    //     type: "DELETE"
-    //   });
-    // }
   };
+
+  var API2 = {
+    getTasks: function() {
+      return $.ajax({
+        url: "/api/tasks",
+        type: "GET"
+      });
+    }
+  };
+
+  function getTask() {
+    API2.getTasks().then(function(taskTable) {
+      console.log(taskTable);
+      taskData = taskTable;
+      return taskData;
+    });
+  }
 
   // refreshUsers gets new users from the db and repopulates the list
   var refreshUsers = function() {
     API.getUsers().then(function(data) {
       console.log(data);
+
       // Here we then log the data to console, where it will show up as an object.
       console.log("------------------------------------");
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-      // Loop through and display each of the customers
-      for (var i = 0; i < data.length; i++) {
-        // Get a reference to the tableList element and populate it with tables
 
-        // Then display the fields in the HTML (Section Name, Date, URL)
+      // Loop through and display each of the customers
+
+      for (var i = 0; i < data.length; i++) {
         var listItem = $("<li class='list-group-item mt-4'>").attr({
           "data-id": data[i].id
         });
-
-        var $h2 = $("<h2>");
-        $h2.append();
+        console.log(taskData[i].task);
 
         listItem.append(
           $(
-            "<h2>User Name: <a href=edit/user/" +
-              data[i].id +
-              ">" +
+            "<h2>User Name: <a href='../edit/user/'>" +
               data[i].userName +
               "</a>"
-          )
+          ),
+          $("<h2> Task: Hello</h2>")
         );
 
         $userList.append(listItem);
       }
     });
   };
-
-  // handleDeleteBtnClick is called when an user's delete button is clicked
-  // Remove the users from the db and refresh the list
-  // var handleDeleteBtnClick = function() {
-  //   var idToDelete = $(this).attr("data-id");
-
-  //   API.deleteUsers(idToDelete).then(function() {
-  //     refreshUsers();
-  //   });
-  // };
-
-  // // Add event listeners to the submit and delete buttons
-  // $userList.on("click", ".delete", handleDeleteBtnClick);
+  getTask();
   refreshUsers();
 });
