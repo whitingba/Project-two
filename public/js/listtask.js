480eaa984ec865f588ca4dc0ab8a34896cd392a8
Phil Mauracher <gradientus@gmail.com>
2019-10-07 18:14:33 -0600

    Task List now working.

--- public/js/listtask.js
+++ public/js/listtask.js
@@ -1,1 +1,39 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  var taskData = $("#tableBody");
+
+  var API = {
+    getTasks: function() {
+      return $.ajax({
+        url: "/api/tasks",
+        type: "GET"
+      });
+    }
+  };
+
+  function listTasks() {
+    API.getTasks().then(function(data) {
+      data = data;
+      for (var i = 0; i < data.length; i++) {
+        var tableRow = $(
+          "<tr><td>" +
+            data[i].id +
+            "</td><td>" +
+            data[i].task +
+            "</td><td>" +
+            data[i].frequency +
+            "</td><td>" +
+            data[i].User.userName +
+            "</td></tr>"
+        );
+        taskData.append(tableRow);
+      }
+    });
+  }
+  // console.log(data);
+  // console.log(data[0].id);
+  // console.log(data[0].task);
+  // console.log(data[0].frequency);
+  // console.log(data[0].User.userName);
+
+  listTasks();
+});
